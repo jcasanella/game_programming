abb7ea9b047bd933772479715bc903897a0863fe
jordi <jordikiter@hotmail.es>
2020-12-28 23:24:28 +0000

    Enable shader and draw triangle

--- Learning/source/private/main.cpp
+++ Learning/source/private/main.cpp
@@ -2,22 +2,31 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void drawTriangle();
+GLuint loadShader();
+GLuint prepareTriangle();
+void drawTriangle(GLuint vertexbuffer);
 
 int main() {
 	const int WIDTH = 800;
 	const int HEIGHT = 600;
 	const char* TITLE = "LearnOpenGL";
 
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	if (!glfwInit()) {
+		cout << "Error initializing glfw" << endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);								// Antialiasing 4
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);					// OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);			// mac os  compability
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	// only new open gl
 
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
 	if (window == NULL) {
@@ -25,7 +34,7 @@ int main() {
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window); 
 
 	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
 		cout << "Failed to init GLAD" << endl;
@@ -34,11 +43,18 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	GLuint vertexBuffer = prepareTriangle();
+	GLuint programId = loadShader();
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(programId);
+
+		drawTriangle(vertexBuffer);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -48,20 +64,59 @@ int main() {
 	return 0;
 }
 
-void drawTriangle() 
+void readFile(const char* fileName)
 {
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+	ifstream inData(fileName);
+	if (inData.is_open()) {
+		inData.seekg(0, ios::end);
+		size_t fileSize = inData.tellg();
+		inData.seekg(0);
+		string content(fileSize + 1, '\0');
+		inData.read(&content[0], fileSize);
+	}
+
+	inData.close();
+}
+
+GLuint prepareTriangle() 
+{
+	GLuint vertexArrayId;
+	glGenVertexArrays(1, &vertexArrayId);
+	glBindVertexArray(vertexArrayId);
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
 	};
 
-	// Define buffer before to send into the vertex shader
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+	return vertexBuffer;
+}
+
+void drawTriangle(GLuint vertexbuffer) 
+{
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(
+		0,                  // same as location in the vertex shader location
+		3,                  // size - it's a vec3 so it contains 3 values
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		3 * sizeof(float),  // stride, where it starts the next vertex
+		(void*)0            // desfase del buffer
+	);
+	// Dibujar el triángulo !
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Empezar desde el vértice 0S; 3 vértices en total -> 1 triángulo
+	glDisableVertexAttribArray(0);
+}
+
+GLuint loadShader()
+{
 	// Build and compile the vertex shader
 	// Version matches with the current Open GL version
 	// Define the input - in this case is aPos
@@ -72,13 +127,13 @@ void drawTriangle()
 		" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 		"}\n";
 
-	unsigned int vertexShader;
+	GLuint vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
 	// Check if vertex shader compilation worked
-	int success;
+	GLint success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
@@ -94,7 +149,7 @@ void drawTriangle()
 		" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\n";
 
-	unsigned int fragmentShader;
+	GLuint fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
@@ -106,6 +161,27 @@ void drawTriangle()
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << endl << infoLog << endl;
 	}
+
+	// Link the different shaders
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		memset(infoLog, 0, 512);
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		cout << "ERROR::SHADERPROGRAM::LINK_FAILED" << endl << infoLog << endl;
+	}
+
+	glDetachShader(shaderProgram, vertexShader);
+	glDetachShader(shaderProgram, fragmentShader);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) 
