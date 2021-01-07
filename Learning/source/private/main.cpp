dd9e78a8b2cf234487cf9ebd64d20b37985f95f1
jordi <jordikiter@hotmail.es>
2021-01-07 11:52:57 +0000

    First version drawing a rectangle

--- Learning/source/private/main.cpp
+++ Learning/source/private/main.cpp
@@ -22,6 +22,8 @@ bool isClicked = false;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+GLuint prepareRectangle();
+void drawRectangle(GLuint VAO);
 GLuint prepareTriangle();
 void drawTriangle(GLuint VAO);
 const char* readFile(const char* fileName);
@@ -70,7 +72,9 @@ int main() {
 	// objects you want to draw, you first generate / configure all the VAOs(and thus the required VBO and
 	// attribute pointers) and store those for later use.The moment we want to draw one of our objects, we
 	// take the corresponding VAO, bind it, then draw the object and unbind the VAO again.
-	GLuint VAO = prepareTriangle();
+	//GLuint VAO = prepareTriangle();
+
+	GLuint VAO = prepareRectangle();
 
 	// First Shader program
 	auto vertex = make_tuple(VERTEX_SHADER_LOCATION, VERTEX_SHADER_ERROR, GL_VERTEX_SHADER);
@@ -92,7 +96,8 @@ int main() {
 		glUseProgram(!isClicked ? programId : programId2);
 
 		// Draw the object
-		drawTriangle(VAO);
+		//drawTriangle(VAO);
+		drawRectangle(VAO);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -121,6 +126,71 @@ const char* readFile(const char* fileName)
 	inData.close();
 
 	return content;
+}
+
+GLuint prepareRectangle()
+{
+	const GLfloat vertex_buffer_data[] = {
+		0.5f, 0.5f, 0.0f,	// Top Right
+		0.5f, -0.5f, 0.0f,	// Bottom Right
+		-0.5f, -0.5f, 0.0f,	// Bottom Left
+		-0.5f, 0.5f, 0.0f	// Top Left
+	};
+
+	const GLuint indexes_data[] = {
+		0, 1, 3,	// First triangle
+		1, 2, 3		// Second triangle
+	};
+
+	// A Vertex Array Object (VAO) is an object which contains one or more Vertex Buffer Objects and is designed to store the information 
+	// for a complete rendered object. Can contain multiples VBO
+	// Contains the following info:
+	// - Calls to glEnableVertexAttribArray or glDisableVertexAttrib Array
+	// - Vertex attribute configurations via glVertexAttribPointer and objects associated via glVertexAttribPointer
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// A Vertex Buffer Object(VBO) is a memory buffer in the GPU designed to hold information about vertices. VBOs can also store information such as normals, 
+	// texcoords, indices, etc.
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+
+	// Copy our vertices array into a buffer to be used by OpenGL
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes_data), indexes_data, GL_STATIC_DRAW);
+
+
+	// Set the vertex attribute pointers
+	glVertexAttribPointer(
+		0,                  // same as location in the vertex shader location
+		3,                  // size - it's a vec3 so it contains 3 values
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		3 * sizeof(float),  // stride, where it starts the next vertex
+		(void*)0            // desfase del buffer
+	);
+
+	glBindVertexArray(0);	// unbind VAO
+
+	return VAO;
+}
+
+void drawRectangle(GLuint VAO)
+{
+	glBindVertexArray(VAO);
+	glEnableVertexAttribArray(0);	// sme as location in the vertex shader
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw Rectangle
+
+	glDisableVertexAttribArray(0);	// sme as location in the vertex shader
+	glBindVertexArray(0);	// this unbinds
 }
 
 GLuint prepareTriangle() 
